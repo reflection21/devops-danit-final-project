7b2461b817c3bb74cacc8b73411353ae2469bb8c
Your Name <you@example.com>
2026-02-18 22:29:16 +0200

    create fastapi and dockerfile

---
+++ python-app/main.py
@@ -0,0 +1,18 @@
+from fastapi import FastAPI # импортируем класс FastAPI из молуля fastapi
+import socket  # модуль для работы с сетью
+
+app = FastAPI() # создаем обьект из класса фастапи
+
+
+@app.get("/")
+def get_ip():
+    try:
+        hostname = socket.gethostname()
+        ip = socket.gethostbyname(hostname)
+    except Exception:
+        ip = "Unable to determine via hostname"
+
+    return {
+        "hostname": hostname,
+        "ip": ip,
+    }
